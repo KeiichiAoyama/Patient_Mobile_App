99f4a9839d6be7421285fb0127dde1844ac09847
Muhammad Ilham Kurniawan <127322172+muhilhamk@users.noreply.github.com>
2025-01-13 18:15:25 +0700

    update resume main

--- app/src/main/java/com/example/patientmobileapp/ResumeMedisActivity.java
+++ app/src/main/java/com/example/patientmobileapp/ResumeMedisActivity.java
@@ -1,24 +1,45 @@
 package com.example.patientmobileapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ToggleButton;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class ResumeMedisActivity extends AppCompatActivity {
+
+    private ToggleButton buttonRawatJalan, buttonTesLab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_resume_medis);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        buttonRawatJalan = findViewById(R.id.button_rawat_jalan);
+        buttonTesLab = findViewById(R.id.button_tes_lab);
+
+        loadFragment(new RawatJalanFragment());
+        buttonRawatJalan.setChecked(true);
+
+        buttonRawatJalan.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                buttonTesLab.setChecked(false);
+                loadFragment(new RawatJalanFragment());
+            }
         });
+
+        buttonTesLab.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                buttonRawatJalan.setChecked(false);
+                loadFragment(new TesLabFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_resume, fragment)
+                .commit();
     }
 }
